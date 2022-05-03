cd098ad2241dc06e656412d9e8c2d7a0d0e1f99d
Chris Sägner <chris.saegner99@web.de>
2022-05-03 11:08:26 +0200

    add timeseries in exchangeapi

--- src/exchangeratesapi.ts
+++ src/exchangeratesapi.ts
@@ -9,6 +9,20 @@ export type ResponseLatest = {
   rates: { [key: string]: number }
 };
 
+export type ResponseTimeseries = {
+  success: boolean,
+  timestamp: number,
+  base: string,
+  date: string,
+  rates: { [key: string]: number }
+};
+
+
+export type TimeseriesOptions = {
+  startDate: Date,
+  dateOffset: number
+};
+
 async function getAsync<T>(ep: string, params?: URLSearchParams): Promise<T> {
   params = params || new URLSearchParams();
   params.append('access_key', API_KEY);
@@ -26,4 +40,23 @@ async function getAsync<T>(ep: string, params?: URLSearchParams): Promise<T> {
 
 export function latestAsync(): Promise<ResponseLatest> {
   return getAsync<ResponseLatest>('latest');
+}
+
+function format(date: Date) {
+  var day = ('0' + date.getDate()).slice(-2);
+  var month = ('0' + (date.getMonth() + 1)).slice(-2);
+  var year = date.getFullYear();
+
+  return year + '-' + month + '-' + day;
+}
+
+export function timeseriesAsync({ startDate, dateOffset }: TimeseriesOptions = { startDate: new Date(), dateOffset: 30 }): Promise<ResponseTimeseries> {
+  startDate = startDate || new Date();
+  const endDate = new Date(startDate);
+  endDate.setDate(endDate.getDate() - dateOffset);
+
+  return getAsync<ResponseTimeseries>('timeseries', new URLSearchParams({
+    start_date: format(startDate),
+    end_date: format(endDate)
+  }));
 }
