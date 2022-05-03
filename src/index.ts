e840dc73a697f0178675ebcd4c94d3692fc2a172
Chris Sägner <chris.saegner99@web.de>
2022-05-03 11:05:33 +0200

    add html + queryselector

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+import { latestAsync } from "./exchangeratesapi";
 
-import { helloWorld, Beispiel } from "./myModule";
-import { alertMe } from "./myOtherModule";
+(async function () {
+  const currencyCode1 = document.querySelector<HTMLSelectElement>("#currency_code1");
+  const currencyCode2 = document.querySelector<HTMLSelectElement>("#currency_code2");
 
-console.log(helloWorld);
-customElements.define("my-beispiel", Beispiel);
+  const number1 = document.querySelector<HTMLInputElement>("#number1");
+  const number2 = document.querySelector<HTMLInputElement>("#number2");
 
-alertMe();
+  if (currencyCode1 && currencyCode2 && number1 && number2) {
+    const latest = await latestAsync();
+    for (var rate in latest.rates) {
+      const option1 = new Option(rate, latest.rates[rate].toString());
+      if (rate === latest.base) {
+        option1.selected = true;
+      }
+      currencyCode1.add(option1);
 
-const myInputValue = document.querySelector<HTMLInputElement>("#myInput");
-
-const myInputValueAlternate = document.querySelector(
-  "#myInput"
-) as HTMLInputElement;
-
-document
-  .querySelector<HTMLInputElement>("#myInput")
-  ?.addEventListener("focus", doSmth);
-
-function doSmth(e: UIEvent) {
-  const val = e.target as HTMLInputElement;
-  console.log(e, val.value);
-}
+      const option2 = new Option(rate, latest.rates[rate].toString());
+      currencyCode2.add(option2);
+    }
+  }
+})();
