d93e6144cdd321f337ba8d13b4efc5d469b29c26
Chris Sägner <chris.saegner99@web.de>
2022-05-05 09:58:05 +0200

    Mapping function highest und lowest value of rates

---
+++ src/number.ts
@@ -0,0 +1,3 @@
+export function map(value: number, in_min: number, in_max: number, out_min: number, out_max: number): number {
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
