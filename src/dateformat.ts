5b9e524d0e7e741ea2dd3cca5110a64ddf1b74c9
Chris Sägner <chris.saegner99@web.de>
2022-05-05 10:02:44 +0200

    outsource dateformat function

---
+++ src/dateformat.ts
@@ -0,0 +1,7 @@
+export function format(date: Date) {
+    var day = ('0' + date.getDate()).slice(-2);
+    var month = ('0' + (date.getMonth() + 1)).slice(-2);
+    var year = date.getFullYear();
+
+    return year + '-' + month + '-' + day;
+}
